a003efc29361dcf41bf139d60332ae9170e5480b
Crowdbotics <team@crowdbotics.com>
2023-04-07 19:50:38 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/black_tooth_39782/MainActivity.java
@@ -0,0 +1,15 @@
+package com.black_tooth_39782;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "black_tooth_39782";
+  }
+}
