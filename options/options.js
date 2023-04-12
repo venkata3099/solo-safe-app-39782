63a64fd3420a7ef2e4c8e7984901b2568afd710a
ivanapfafsovljanski <ivana.pfaf@crowdbotics.com>
2023-04-12 20:56:45 +0200

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,12 +1,11 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
-  name: "black-tooth-39782",
-  url: "https://black-tooth-39782.botics.co",
-  api: "https://black-tooth-39782.botics.co/api/v1"
-}
+  "name": "black-tooth-39782",
+  "url": "https://black-tooth-39782.botics.co",
+  "api": "https://black-tooth-39782.botics.co/api/v1",
+  "initialRoute": "Splash"
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
