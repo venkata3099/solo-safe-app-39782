1a9d20f24c0bda6425b1a1061710ee813836a064
Crowdbotics <team@crowdbotics.com>
2023-05-11 03:07:03 +0000

    Add

--- screens/index.js
+++ screens/index.js
@@ -1,275 +1,125 @@
-import React, { useState } from "react";
-import {
-  Text,
-  View,
-  StyleSheet,
-  TextInput,
-  ScrollView,
-  Pressable,
-  Image
-} from "react-native";
+import React from "react";
+import { Text, View, StyleSheet, TouchableHighlight, TextInput } from "react-native";
 
-const SubscriptionTrial = () => {
-  const [startDate, setStartDate] = useState("");
-  const [endDate, setEndDate] = useState("");
+const SupportSendFeedbackScreen = () => {
   return (
     <View style={styles.container}>
-      <ScrollView>
-        <Text style={styles.heading}>Free tiral period</Text>
-        <View style={styles.daysContainer}>
-          <Text style={styles.days}>30</Text>
-          <Text>Days</Text>
-        </View>
-        <Text style={styles.description}>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Non at sed.
+      <View>
+        <Text style={styles.labelText}>
+          Email address
         </Text>
-        <View style={styles.separator}>
-          <View style={styles.bar} />
-          <Text style={styles.separatorText}>Or</Text>
-          <View style={styles.bar} />
-        </View>
-        <Text style={styles.periodHeading}>Choose your period</Text>
-        <View style={styles.inputsContainer}>
-          <Input
-            text="Start date"
-            value={startDate}
-            onChange={setStartDate}
-            containerStyle={styles.input}
-          />
-          <Input
-            text="End date"
-            value={endDate}
-            onChange={setEndDate}
-            containerStyle={styles.input}
-          />
-        </View>
-        <Button buttonText="Ok" style={styles.button} />
-      </ScrollView>
-    </View>
-  );
-};
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff"
-  },
-  heading: {
-    fontSize: 28,
-    fontWeight: "bold",
-    color: "#000",
-    textAlign: "center"
-  },
-  daysContainer: {
-    width: 130,
-    height: 130,
-    borderRadius: 70,
-    borderColor: "#12D790",
-    borderWidth: 12,
-    justifyContent: "center",
-    alignItems: "center",
-    alignSelf: "center",
-    marginVertical: 20
-  },
-  days: {
-    fontSize: 36,
-    fontWeight: "bold"
-  },
-  description: {
-    fontSize: 14,
-    color: "#888888",
-    textAlign: "center",
-    paddingHorizontal: 40
-  },
-  separator: {
-    flexDirection: "row",
-    justifyContent: "center",
-    alignItems: "center",
-    marginVertical: 20
-  },
-  bar: {
-    width: 100,
-    height: 1,
-    backgroundColor: "#e6e6e6"
-  },
-  separatorText: {
-    fontSize: 14,
-    color: "#888888",
-    marginHorizontal: 30
-  },
-  periodHeading: {
-    fontSize: 18,
-    color: "#000",
-    textAlign: "center",
-    marginVertical: 10,
-    fontWeight: "bold"
-  },
-  inputsContainer: {
-    flexDirection: "row",
-    justifyContent: "center",
-    padding: 20
-  },
-  input: {
-    flex: 1,
-    marginHorizontal: 5
-  },
-  button: {
-    marginHorizontal: 40,
-    marginTop: 60,
-    marginBottom: 20
-  }
-});
+        <Input
+          placeholder='Enter'
+        />
+      </View>
+      <View style={styles.Textarea}>
+        <Text style={styles.labelText}>
+          Message
+        </Text>
+        <TextInput style={styles.TextAreaInput}
+          numberOfLines={5}
+          multiline={true}
+          placeholder='Enter'
+          placeholderTextColor='#ddd'
+        />
+      </View>
 
-export default SubscriptionTrial;
-
-const Input = props => {
-  return (
-    <View style={[inputStyles.inputContainer, props.containerStyle]}>
-      {props.text
-        ? (
-        <Text style={inputStyles.inputText}>{props.text}</Text>
-          )
-        : null}
-
-      <TextInput
-        style={[
-          inputStyles.input,
-          props.style,
-          props.textArea ? inputStyles.textArea : null
-        ]}
-        placeholder={props.placeholder ? props.placeholder : "Enter"}
-        value={props.value}
-        onChangeText={() => props.onChange()}
-        placeholderTextColor={
-          props.placeholderTextColor ? props.placeholderTextColor : "#9B9B9B"
-        }
-        editable={props.editable !== false}
-        autoCapitalize="none"
-        autoCorrect={false}
-        multiline={!!props.textArea}
-        backgroundColor={props.backgroundColor}
-        secureTextEntry={props.secureTextEntry}
-      />
-      {props.errorText
-        ? (
-        <Text style={inputStyles.error}>{props.errorText}</Text>
-          )
-        : null}
-      {props.icon
-        ? (
-        <Pressable
-          onPress={() => props.iconOnPress()}
-          style={inputStyles.iconWithText}>
-          <Image source={props.icon} style={inputStyles.icon} />
-        </Pressable>
-          )
-        : null}
-      <View style={styles.children}>{props.children}</View>
-    </View>
-  );
-};
-
-const inputStyles = StyleSheet.create({
-  inputContainer: {
-    flexDirection: "column",
-    justifyContent: "center"
-  },
-  inputText: {
-    fontSize: 14,
-    marginLeft: 20,
-    color: "#111112"
-  },
-  input: {
-    borderWidth: 1,
-    borderColor: "#e6e6e6",
-    borderRadius: 10,
-    padding: 10,
-    paddingLeft: 20,
-    marginVertical: 10,
-    width: "100%",
-    height: 50,
-    color: "#000"
-  },
-  iconWithText: {
-    position: "absolute",
-    right: 30,
-    bottom: 25,
-    width: 20,
-    height: 20,
-    justifyContent: "center",
-    alignItems: "center",
-    zIndex: 1
-  },
-  icon: {
-    width: "100%",
-    height: "100%",
-    resizeMode: "contain"
-  },
-  textArea: {
-    height: 150
-  },
-  children: {}
-});
-
-const Button = params => {
-  const backgroundColor = params.backgroundColor || "#000";
-  const textColor = params.textColor || "#fff";
-  const btnStyle = {
-    backgroundColor: backgroundColor,
-    borderColor: params.borderColor || backgroundColor,
-    borderWidth: 1
-  };
-  const btnText = {
-    color: textColor
-  };
-  return (
-    <View style={[buttonStyles.btnContainer, params.style]}>
-      <View style={!params.hideShadow ? buttonStyles.shadowContainer : null}>
-        <Pressable
-          style={[buttonStyles.btn, btnStyle]}
-          onPress={params.onPress}>
-          <Text style={[buttonStyles.btnText, btnText]}>
-            {params.buttonText}
-          </Text>
-          <View style={styles.childrenContainer}>{params.children}</View>
-        </Pressable>
+      <View style={styles.SubmitBtn}>
+        <Button>Submit</Button>
       </View>
     </View>
   );
 };
 
-const buttonStyles = StyleSheet.create({
-  btnContainer: {
-    justifyContent: "center"
+const styles = StyleSheet.create({
+  container: {
+    padding: 20,
+    flex: 1,
+    height: "100%",
+    backgroundColor: "white"
   },
-  shadowContainer: {
-    shadowColor: "rgba(0, 0, 0, 0.5)",
-    shadowOffset: {
-      width: 0,
-      height: 5
-    },
-    shadowOpacity: 0.5,
-    shadowRadius: 10,
-    elevation: 10,
-    backgroundColor: "#fff",
-    borderRadius: 10
+  labelText: {
+    fontSize: 14,
+    fontWeight: "400",
+    marginBottom: 10,
+    marginLeft: 15
   },
-  btn: {
-    height: 50,
-    padding: 10,
-    paddingHorizontal: 25,
+  Textarea: {
+    marginTop: 30
+  },
+  TextAreaInput: {
+    height: 160,
+    borderWidth: 1,
+    borderColor: "#C4C4C4",
     borderRadius: 10,
+    textAlignVertical: "top",
+    padding: 10
+  },
+  SubmitBtn: {
+    marginTop: 100
+  }
+});
+
+export default SupportSendFeedbackScreen;
+
+const Button = (props) => {
+  return (
+    <TouchableHighlight onPress={props.onPress} underlayColor='#DDDDDD'>
+      <View style={[btnStyles.button, {
+        backgroundColor: props.backgroundColor ? props.backgroundColor : "#000000",
+        height: props.height ? props.height : 49,
+        borderWidth: props.borderWidth ? props.borderWidth : 0,
+        borderColor: props.borderColor ? props.borderColor : "#000000"
+      }]}>
+        <Text style={[btnStyles.text, { color: props.color ? props.color : "#ffffff" }]}>{props.children}</Text>
+      </View>
+    </TouchableHighlight>
+  );
+};
+
+const btnStyles = StyleSheet.create({
+  button: {
+    display: "flex",
     justifyContent: "center",
     alignItems: "center",
+    borderRadius: 10
+  },
+  text: {
+    fontWeight: "bold",
+    fontSize: 15
+  }
+});
 
-    flexDirection: "row"
+const Input = (props) => {
+  return (
+    <View>
+      <TextInput
+        style={textStyles.input}
+        placeholder={props.placeholder}
+        value={props.value}
+        onChangeText={(num) => props.setValue(num)}
+        placeholderTextColor='#ddd'
+        editable={props.editable !== false}
+      />
+      {props.errorText ? <Text style={textStyles.error}>{props.errorText}</Text> : null}
+    </View>
+  );
+};
+
+const textStyles = StyleSheet.create({
+  input: {
+    backgroundColor: "#fff",
+    height: 53,
+    borderColor: "#C4C4C4",
+    color: "#000",
+    borderRadius: 10,
+    fontSize: 14,
+    borderWidth: 1,
+    paddingHorizontal: 10
   },
-  btnText: {
-    color: "#fff",
-    fontSize: 16,
-    fontWeight: "bold"
-  },
-  childrenContainer: {
-    justifyContent: "center",
-    alignItems: "center"
+  error: {
+    fontSize: 13,
+    color: "#FA060D",
+    paddingTop: 8
   }
 });
